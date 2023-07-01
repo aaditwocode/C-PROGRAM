eec3c28238499f378a055d83c0e46e162ac78387
aaditwocode <137618627+aaditwocode@users.noreply.github.com>
2023-07-01 20:51:09 +0530

    Add files via upload

---
+++ instructions.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+ int main()
+ {
+    /// type declaration function
+    int a=4 ;
+    int b=2;
+    // printf("the value of a is %d\n" , a);
+    // printf("the value of b is %d\n" , b);
+    
+    // float a=1.1;
+    // float b=a+8.9;
+//  printf("the value of a + b is %d\n" , a + b);
+//  printf("the value of a - b is %d\n" , a - b);
+//  printf("the value of a * b is %d\n" , a * b);
+//  printf("the value of a / b is %d\n" , a / b);
+printf("the remainder when 5 is divided by 2 is %d" , 5%2);
+printf("the value of 3.0 /9 is %f" , 3.0/9);
+    return 0;
+}
---
+++ operator.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+    int main(){
+    int x= 1;
+    int y = 2;
+   printf("the value of 3x-8y is %d\n" , 3*x-8*y) ;
+   printf("the value of 3x/8y is %d\n" , 3*x  / 8*y) ;
+    return 0;
+}
