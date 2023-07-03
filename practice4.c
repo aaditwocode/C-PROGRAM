19157075f16b46ad951999da43648b6fce6a69f7
aaditwocode <137618627+aaditwocode@users.noreply.github.com>
2023-07-03 13:25:50 +0530

    Add files via upload

---
+++ practice4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+    int main(){
+        // int a;
+        // int b=a;
+        // int v=3^3;
+        // float d = (3.0/8 -2);
+        // printf("%f\n", d);
+        // printf("%d\n", v);
+
+        int num;
+        printf("enter number");
+        scanf("%d", &num);
+
+        printf("divisibility test of 97 is %d" , num%97);
+
+    return 0;
+}
