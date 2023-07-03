a7f51b990984e701e8ef54aafa3dfdd6f4a1a2f7
aaditwocode <137618627+aaditwocode@users.noreply.github.com>
2023-07-03 23:25:27 +0530

    Add files via upload

---
+++ ifelse.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+    int main(){
+        int age;
+        printf("enter your age\n");
+        scanf("%d", &age);
+
+        if (age>=69)
+        {
+            printf("you are not eligible to drive anymore\n");
+        }
+        else
+        {
+            printf("you can drive");
+        }
+        
+        
+    
+    return 0;
+}
