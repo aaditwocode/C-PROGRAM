9bc2269fe92eaddfe40cb8792ec6934ef1b7c76e
aaditwocode <137618627+aaditwocode@users.noreply.github.com>
2023-07-05 13:43:33 +0530

    Add files via upload

---
+++ dowhile.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+    int main(){
+        int a=0;
+        do
+        {
+            printf("the value of a is %d\n" , a);
+            a++;
+        } while (a<20);
+        
+        
+        
+    return 0;
+}
---
+++ quiz2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+    int main(){
+        int i;
+        int n;
+        printf("enter n");
+        scanf("%d" , &n);
+        for ( i=n; i; i--)
+        {
+            printf("enter i is %d\n" , i);
+        }
+        
+        
+    return 0;
+}
